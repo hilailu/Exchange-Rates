fbebae48aa2b977c7c519181b9cfbb31560ad86c
Ekaterina <hilailu37@gmail.com>
2023-08-09 17:12:14 +0300

    Implement reading dates from Excel file and writing respective exchange rates

--- Exchange Rates/Program.cs
+++ Exchange Rates/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using GemBox.Spreadsheet;
+using System.Text.Json;
 
 internal class Program
 {
@@ -13,6 +14,8 @@ internal class Program
 
     static async Task Main(string[] args)
     {
+        SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
+
         Console.WriteLine("Welcome! Here you can find exchange rates for USD and EUR for specific dates.");
 
         while (true)
@@ -27,7 +30,7 @@ internal class Program
                     await InputDateAndDisplayExchangeRates();
                     break;
                 case "2":
-                    // Excel logic
+                    await InputExcelPathAndWriteExchangeRates();
                     break;
                 default:
                     Console.WriteLine("Please select a valid option.");
@@ -80,6 +83,70 @@ internal class Program
         DisplayExchangeRates(exchangeRates);
     }
 
+    private static async Task InputExcelPathAndWriteExchangeRates()
+    {
+        Console.Write("Enter the full path to the Excel file: ");
+        string excelFilePath = Console.ReadLine();
+
+        if (File.Exists(excelFilePath))
+        {
+            await ReadDatesFromExcelAndWriteExchangeRates(excelFilePath);
+        }
+        else
+        {
+            Console.WriteLine("Path is not valid.");
+        }
+    }
+
+    private static async Task ReadDatesFromExcelAndWriteExchangeRates(string path)
+    {
+        ExcelFile file = ExcelFile.Load(path);
+        ExcelWorksheet ws = file.Worksheets[0];
+
+        // Assuming that the dates are in the first column
+        int dateColumnIndex = 0;
+
+        // Offset for writing in the exchange rates
+        int columnOffset = 1;
+
+        // Insert header row with content names (assuming the file has only dates)
+        ws.Rows.InsertEmpty(0, 1);
+        ws.Cells[0, dateColumnIndex].Value = "Date";
+        for (int i = 0; i < currencyAbbreviations.Length; i++)
+        {
+            ws.Cells[0, i + columnOffset].Value = currencyAbbreviations[i];
+        }
+
+        // Start from 1st row to skip header
+        for (int rowIndex = 1; rowIndex < ws.Rows.Count; rowIndex++)
+        {
+            string dateValue = ws.Cells[rowIndex, dateColumnIndex].Value?.ToString();
+            string parsedDate = ParseDate(dateValue);
+
+            // If the date is invalid, skip the row
+            if (!string.IsNullOrEmpty(parsedDate))
+            {
+                List<ExchangeRate>? exchangeRates = await GetExchangeRateForDate(parsedDate);
+
+                if (exchangeRates != null)
+                {
+                    foreach (var exRate in exchangeRates)
+                    {
+                        decimal exchangeRate = exRate.Cur_OfficialRate;
+
+                        // Get column index to insert the value
+                        int columnIndex = Array.IndexOf(currencyAbbreviations, exRate.Cur_Abbreviation) + columnOffset;
+
+                        ws.Cells[rowIndex, columnIndex].Value = exchangeRate;
+                    }
+                }
+            }
+        }
+
+        file.Save(path);
+        Console.WriteLine("File modified successfully.");
+    }
+
     private static void DisplayExchangeRates(List<ExchangeRate>? exchangeRates)
     {
         foreach (var exRate in exchangeRates)
@@ -93,14 +160,28 @@ internal class Program
         while (true)
         {
             Console.Write("Enter a date: ");
-            if (DateTime.TryParse(Console.ReadLine(), out DateTime parsedDate))
+            string date = ParseDate(Console.ReadLine());
+            if (!string.IsNullOrEmpty(date))
             {
-                return parsedDate.ToUniversalTime().ToString("yyyy-MM-dd");
+                return date;
             }
             else
             {
                 Console.WriteLine("Invalid input. Please try again.");
             }
+        }
+    }
+
+    private static string ParseDate(string inputDate)
+    {
+        if (DateTime.TryParse(inputDate, out DateTime parsedDate))
+        {
+            return parsedDate.ToUniversalTime().ToString("yyyy-MM-dd");
+        }
+        else
+        {
+            // Parsing failed
+            return string.Empty;
         }
     }
 }
