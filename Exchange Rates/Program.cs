c0c2a5e9b316d0cecf5a55fea38c02c8606ca361
Ekaterina <hilailu37@gmail.com>
2023-08-09 14:55:00 +0300

    Implement date input and exchange rate requests

--- Exchange Rates/Program.cs
+++ Exchange Rates/Program.cs
@@ -1,7 +1,48 @@
 ﻿internal class Program
 {
-    private static void Main(string[] args)
+    private static readonly string baseUrl = "https://api.nbrb.by/";
+    private static readonly string[] currencyCodes = { "USD", "EUR" };
+
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome! Here you can find exchange rates for USD and EUR for specific dates.");
+
+        string inputDate = InputDate();
+
+        using (HttpClient client = new HttpClient())
+        {
+            // separate requests for currencies
+            foreach (string code in currencyCodes)
+            {
+                string completeUrl = $"{baseUrl}ExRates/Rates/{code}?onDate={inputDate}&ParamMode=2";
+
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(completeUrl);
+                    response.EnsureSuccessStatusCode();
+
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseBody);
+                }
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
+            }
+        }
+    }
+
+    private static string InputDate()
+    {
+        Console.Write("Enter a date: ");
+        bool isDateValid = DateTime.TryParse(Console.ReadLine(), out DateTime parsedDate);
+
+        if (!isDateValid)
+        {
+            Console.WriteLine("Invalid input. Please try again.");
+            InputDate();
+        }
+
+        return parsedDate.ToUniversalTime().ToString("yyyy-MM-dd");
     }
 }
