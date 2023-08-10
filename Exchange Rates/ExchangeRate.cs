a8abbc2710c1c2263c168daf4fb56955488cdce1
Ekaterina <hilailu37@gmail.com>
2023-08-10 14:28:00 +0300

    Add DateExchangeRate class and move writing data to Excel file to a separate method

--- Exchange Rates/ExchangeRate.cs
+++ Exchange Rates/ExchangeRate.cs
@@ -10,4 +10,14 @@ namespace ExchangeRates
         [JsonPropertyName("Cur_OfficialRate")]
         public decimal OfficialRate { get; set; }
     }
+
+    public class DateExchangeRate
+    {
+        public Guid Id { get; set; }
+
+        public string Date { get; set; }
+
+        public decimal USDRate { get; set; }
+        public decimal EURRate { get; set; }
+    }
 }
